71c59f0241f604ef203c3f5b1afdb97c51e5d4d1
Matt Stine <matt.stine@gmail.com>
2018-03-23 10:56:41 -0500

    create load balanced RestTemplate

--- fleet-location-updater/src/main/java/demo/FleetLocationUpdaterApplication.java
+++ fleet-location-updater/src/main/java/demo/FleetLocationUpdaterApplication.java
@@ -19,6 +19,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Main entry point for the Fleet Location Updater application.
@@ -35,4 +38,9 @@ public class FleetLocationUpdaterApplication {
 		SpringApplication.run(FleetLocationUpdaterApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
